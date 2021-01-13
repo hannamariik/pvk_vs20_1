7d105b5d7bf2d6d5c28a1e7dc3d79aca6e72afa3
hannamariik <71319264+hannamariik@users.noreply.github.com>
2021-01-13 08:45:17 +0200

    Add files via upload

---
+++ app/controllers/Pages.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Pages
+{
+
+    /**
+     * Pages constructor.
+     */
+    public function __construct()
+    {
+        echo 'Pages class is loaded<br>';
+    }
+
+    public function index(){
+        echo 'index method is loaded<br>';
+    }
+
+    public function about(){
+        echo 'about method is loaded<br>';
+    }
+}
